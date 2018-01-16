866bd0632d35bddd4f40885c3296ee58875bed1c
shafi-khan <34467669+shafi-khan@users.noreply.github.com>
2018-01-16 14:06:38 -0500

    Inventory Managment System
    Inventory Managment System

---
+++ test.py
@@ -0,0 +1,12 @@
+#!C:\Program Files (x86)\Python36-32\python.exe
+
+import pymysql
+import cgi
+
+print('Content-type: text/html\n')
+print('<html>')
+print('<head><title>Hello World</title></head>')
+print('<body>')
+print('<h2>Hello World.</h2>')
+print('</body>')
+print('</html>')
